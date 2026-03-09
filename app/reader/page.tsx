a52aa2f9382ea991ac893f4c5d7899bb0adb2aef
PlasmaR1 <zachzou@foxmail.com>
2026-03-09 14:25:22 +1100

    Add i-frame reader

---
+++ app/reader/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function ReaderPage() {
+  return (
+    <iframe
+      src="https://book4.bigwindvi.com/2411/03zyz/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none"
+      }}
+    />
+  );
+}
